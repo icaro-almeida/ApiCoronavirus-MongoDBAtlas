a4c83754e0c1bc074cc4b52ed20183d5b69a97a4
Icaro <icaro.almeida@engenharia.ufjf.br>
2021-06-02 00:50:15 -0300

    Add project files.

---
+++ Controllers/InfectadoController.cs
@@ -0,0 +1,74 @@
+﻿using Api.Data.Collections;
+using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+
+            return Ok(infectados);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult AtualizarInfectado(string id, [FromBody] InfectadoDto dto)
+        {
+            try
+            {
+                var filter = Builders<Infectado>.Filter.Eq("Id", ObjectId.Parse(id));
+                //var update = Builders<BsonDocument>.Update.Set("class_id", 483);
+                //_infectadosCollection.UpdateOne(Builders<Infectado>.Filter.Where(i => i.Id == ObjectId.Parse(id)), Builders<Infectado>.Update.Set("sexo", dto.Sexo));
+
+                var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+                infectado.Id = ObjectId.Parse(id).ToString();
+
+                var replaceOneResult = _infectadosCollection.ReplaceOneAsync(
+                    i => i.Id == ObjectId.Parse(id).ToString(),
+                    infectado);
+
+                return Ok("Atualizado com sucesso!");
+            }
+            catch
+            {
+                return StatusCode(400, "A atualização falhou!");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+
+            _infectadosCollection.DeleteOne(Builders<Infectado>.Filter.Where(i => i.Id == ObjectId.Parse(id).ToString()));
+
+            return StatusCode(200, "Excluído com sucesso!");
+        }
+    }
+}
---
+++ Data/Collections/Infectado.cs
@@ -0,0 +1,26 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Driver.GeoJsonObjectModel;
+
+namespace Api.Data.Collections
+{
+    public class Infectado
+    {
+        public Infectado(DateTime dataNascimento, string sexo, double latitude, double longitude)
+        {
+            this.Id = ObjectId.GenerateNewId().ToString();
+            this.DataNascimento = dataNascimento;
+            this.Sexo = sexo;
+            this.Localizacao = new GeoJson2DGeographicCoordinates(longitude, latitude);
+        }
+
+        [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } 
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public GeoJson2DGeographicCoordinates Localizacao { get; set; }
+    }
+}
